873f9f9954049006079e38f36fbf7ee1f20bc78e
RealGachoka <69094728+RealGachoka@users.noreply.github.com>
2024-09-28 08:26:01 +0300

    Files

---
+++ app/src/main/java/com/example/NourishConnect/ReadWriteUserDetails.java
@@ -0,0 +1,9 @@
+package com.example.NourishConnect;
+
+public class ReadWriteUserDetails {
+    public String mobile;
+    public ReadWriteUserDetails(){};
+    public ReadWriteUserDetails( String textMobile){
+        this.mobile=textMobile;
+    }
+}
